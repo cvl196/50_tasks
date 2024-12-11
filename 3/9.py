b1e5708aa0d07d6764345e81db81df4bc20e3147
cvl196 <sfilipp2008@yandex.ru>
2024-12-11 08:00:54 +0500

    update 9.py

--- 3/9.py
+++ 3/9.py
@@ -1,11 +1,23 @@
 def lucky_ticket(n):
     n = n/2
     n = int(n)
-    sum = 0
+    suma = 0
+    sum_i = 0
+    sum_j = 0
     for i in range (0,10**n):
         for j in range (0,10**n):
-            if i == j and i != 0:
-                sum += 1
-    return sum
+            i = str (i)
+            j = str (j)
+            for num in i:
+                sum_i += int(num)
+            for num in j:
+                sum_j += int(num)
+            if sum_j == sum_i:
+                suma += 1
+            sum_i = 0
+            sum_j = 0
 
-print(lucky_ticket(6))
+    return suma
+
+print(lucky_ticket(4))
+
