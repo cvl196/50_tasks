021192f6323b3a483068e0d3e8cb713e82e5c63b
cvl196 <123945470+cvl196@users.noreply.github.com>
2024-12-27 22:34:52 +0500

    Update 9.py

--- 2/9.py
+++ 2/9.py
@@ -2,7 +2,7 @@ def del_enem(people,enemies):
     res = []
     for enemy in enemies:
         for i in range (0,len(people)-1):
-            if people[i] != enemy:
+            if people[i] != enemy and people[i] not in res :
                 res.append(people[i])
 
     return res
